40488b7e531bb65a7698c15f22f93abbaa1e3795
JohnRSantamaria <santamariajohnd@gmail.com>
2025-11-17 22:06:41 -0500

    feature: update logos in the app

--- app/(public)/login/page.tsx
+++ app/(public)/login/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import { LoginForm } from "@/components/auth/login_form"
 import Isologo from "@/components/common/isologo"
-import Logotipo from "@/components/common/logotipo"
+import Isotipo from "@/components/common/isotipo"
 import { useAuth } from "@/hooks/use-auth"
 
 export default function LoginPage() {
@@ -11,8 +11,8 @@ export default function LoginPage() {
 		<div className="grid min-h-svh lg:grid-cols-2">
 			<div className="flex flex-col gap-4 p-6 md:p-10">
 				<div className="flex flex-1 items-center justify-center">
-					<div className="w-full max-w-xs flex flex-col gap-4">
-						<Logotipo className="w-96 h-auto lg:hidden" />
+					<div className="w-full max-w-xs flex flex-col justify-center gap-4">
+						<Isotipo className="h-auto lg:hidden w-52 ml-auto mr-auto" />
 						<LoginForm
 							onSubmit={async (email, password) => {
 								await login(email, password)
@@ -26,7 +26,7 @@ export default function LoginPage() {
 			</div>
 			<div className="bg-muted relative hidden lg:block">
 				<div className="w-full h-full flex justify-center items-center">
-					<Isologo className="w-96 h-auto" />
+					<Isologo className="w-[28rem] h-auto" />
 				</div>
 			</div>
 		</div>
